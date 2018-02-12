ff54aa80b421042e3c416260ede89978f6320055
mvcjunior <33207454+mvcjunior@users.noreply.github.com>
2018-02-12 10:55:22 -0200

    Commit versão Fev/2018

--- src/main/java/br/com/all/codegenerator/dominios/Definicao.java
+++ src/main/java/br/com/all/codegenerator/dominios/Definicao.java
@@ -1,1 +1,49 @@
-Inicial
+package br.com.all.codegenerator.dominios;
+
+public class Definicao {
+	private String tipo;
+	private String nome;
+	private String classeTipoPrimitivo;
+	private String classeTipoPrimitivoRetorno;
+	private String valor;
+	
+	public String getTipo() {
+		return tipo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public String getClasseTipoPrimitivo() {
+		return classeTipoPrimitivo;
+	}
+	public String getClasseTipoPrimitivoRetorno() {
+		return classeTipoPrimitivoRetorno;
+	}
+	public String getValor() {
+		return valor;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public void setClasseTipoPrimitivo(String classeTipoPrimitivo) {
+		this.classeTipoPrimitivo = classeTipoPrimitivo;
+	}
+	public void setClasseTipoPrimitivoRetorno(String classeTipoPrimitivoRetorno) {
+		this.classeTipoPrimitivoRetorno = classeTipoPrimitivoRetorno;
+	}
+	public void setValor(String valor) {
+		this.valor = valor;
+	}
+
+	@Override
+	public String toString() {
+		return "{ tipo: " + tipo + "," 
+			+ " nome: " + nome + ","
+			+ " classeTipoPrimitivo: " + classeTipoPrimitivo + ","
+			+ " classeTipoPrimitivoRetorno: " + classeTipoPrimitivoRetorno + "," 
+			+ " valor: " + valor + "}";
+	}
+}
